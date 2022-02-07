a3c944288ac4be7aa9c5cbe4a61f2f49dbcee1b5
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-02-07 18:25:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function changeTheme()
+{
+document.getElementById("body").style.backgroundColor="orange";
+document.getElementById("body").style.color="green";
+}
+function btnLeave()
+{
+document.getElementById("body").style.backgroundColor="green";
+document.getElementById("body").style.color="black";
+}
+function imgOver()
+{
+document.getElementById("img").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMgzU5xqYPq4FIETHRvsjvXFVLEVeX0tyAhkjMrWEkFERaoBh_X4V1heNCIaC_qY0SftI&usqp=CAU"
+}
+function imgLeave()
+{
+document.getElementById("img").src="https://www.pedestrian.tv/content/uploads/2021/07/27/pokemon1.jpeg"
+}
